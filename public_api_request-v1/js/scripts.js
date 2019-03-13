ad2610ac7a5e9f25cd388d3576cec33336fa3a91
Austin Scott <austin.scott18@gmail.com>
2019-03-12 22:01:06 -0400

    Display gallery of users

--- public_api_request-v1/js/scripts.js
+++ public_api_request-v1/js/scripts.js
@@ -2,7 +2,7 @@
  * Helper Variables *
  ********************/
 const numUsers = 12;
-const users = [];
+let users = [];
 /********************
  *   Fetch Methods  *
  ********************/
@@ -10,19 +10,35 @@ function fetchData(url) {
 	return fetch(url).then((res) => res.json());
 }
 
-function fetchUsers(numUsers) {
-	for (i = 0; i < numUsers; i++) {
-		fetchData('https://randomuser.me/api/')
-			.then((data) => {
-				user = data.results[0];
-				users.push(user);
-			})
-			.catch(() => console.log('Something went wrong.'));
-	}
+function fetchUsers() {
+	fetchData(`https://randomuser.me/api/?results=${numUsers}&nat=us`)
+		.then((data) => (users = data.results))
+		.then((users) => displayGallery(users));
 }
+fetchUsers();
+
+/********************
+ *  Event Listeners *
+ ********************/
 
 /********************
  *   Helper Methods *
  ********************/
+function displayGallery(users) {
+	let galleryHTML = '';
 
-fetchUsers(numUsers);
+	users.forEach((user) => {
+		galleryHTML += `<div class="card">
+                            <div class="card-img-container">
+                                <img class="card-img" src=${user.picture.thumbnail} alt="profile picture">
+                            </div>
+                            <div class="card-info-container">
+                                <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+                                <p class="card-text">${user.email}</p>
+                                <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
+                            </div>
+                        </div>`;
+	});
+
+	document.querySelector('#gallery').innerHTML = galleryHTML;
+}
