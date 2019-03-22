6a63b45e44c28e7c44dce5be14e4958238d7a108
Austin Scott <austin.scott18@gmail.com>
2019-03-21 23:23:18 -0400

    Add search functionality, prev and next modal buttons, css color changes

--- public_api_request-v1/js/scripts.js
+++ public_api_request-v1/js/scripts.js
@@ -21,6 +21,7 @@ fetchUsers();
 /********************
  *  Event Listeners *
  ********************/
+//Sets event listeners related to user cards
 function setCardListeners() {
 	const cards = document.querySelectorAll('.card');
 	cards.forEach((card) => {
@@ -29,12 +30,39 @@ function setCardListeners() {
 		});
 	});
 }
-
+//Sets event listeners related to modal
 function setModalListeners() {
+	const cards = document.querySelectorAll('.card');
 	const modal = document.querySelector('.modal-container');
+	const modalName = modal.querySelector('#name').textContent;
 	const closeButton = document.querySelector('.modal-close-btn');
+	const prevButton = document.querySelector('#modal-prev');
+	const nextButton = document.querySelector('#modal-next');
 	closeButton.addEventListener('click', function(event) {
 		modal.parentElement.removeChild(modal);
+	});
+	prevButton.addEventListener('click', function(event) {
+		for (let i = 0; i < cards.length; i++) {
+			let cardName = cards[i].querySelector('#name').textContent;
+			if (cards[i - 1] !== undefined) {
+				if (cardName === modalName) {
+					modal.parentElement.removeChild(modal);
+					displayModal(cards[i - 1]);
+				}
+			}
+		}
+	});
+
+	nextButton.addEventListener('click', function(event) {
+		for (let i = 0; i < cards.length; i++) {
+			let cardName = cards[i].querySelector('#name').textContent;
+			if (cards[i + 1] !== undefined) {
+				if (cardName === modalName) {
+					modal.parentElement.removeChild(modal);
+					displayModal(cards[i + 1]);
+				}
+			}
+		}
 	});
 }
 
@@ -62,11 +90,12 @@ function displayGallery(users) {
 	gallery.innerHTML = galleryHTML;
 	setCardListeners();
 }
-
+//Displays modal with additional user info
 function displayModal(card) {
 	const cardName = card.querySelector('#name').textContent;
 	let modalDIV = document.createElement('div');
 	modalDIV.className = 'modal-container';
+	modalDIV.innerHTML = '';
 	users.forEach((user) => {
 		if (`${user.name.first} ${user.name.last}` === cardName) {
 			modalDIV.innerHTML = generateModalHTML(user);
@@ -75,7 +104,7 @@ function displayModal(card) {
 	document.body.appendChild(modalDIV);
 	setModalListeners();
 }
-
+//Generates HTML to be displayed in modal
 function generateModalHTML(user) {
 	const birthday = new Date(user.dob.date);
 
@@ -92,5 +121,34 @@ function generateModalHTML(user) {
 		.state} ${user.location.postcode}</p>
                     <p class="modal-text">Birthday: ${birthday.getMonth()}/${birthday.getDay()}/${birthday.getFullYear()}</p>
             </div>
+            <div class="modal-btn-container">
+                <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                <button type="button" id="modal-next" class="modal-next btn">Next</button>
+            </div>
     `;
 }
+
+function generateSearch() {
+	let searchContainer = document.querySelector('.search-container');
+	let searchHTML = '';
+	searchHTML += "<input type='search' id='search-input' class='search-input' placeholder='Search...'>";
+	searchHTML += "<input type='submit' value='&#x1F50D;' id='search-submit' class='search-submit'>";
+
+	let searchForm = document.createElement('form');
+	searchForm.action = '#';
+	searchForm.method = 'get';
+	searchForm.innerHTML = searchHTML;
+	searchContainer.appendChild(searchForm);
+
+	searchForm.addEventListener('input', function(event) {
+		let searchInput = document.querySelector('#search-input').value;
+		let filteredUsers = [];
+		users.forEach((user) => {
+			if (`${user.name.first} ${user.name.last}`.includes(searchInput)) {
+				filteredUsers.push(user);
+			}
+		});
+		displayGallery(filteredUsers);
+	});
+}
+generateSearch();
